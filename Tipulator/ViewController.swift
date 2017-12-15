af13a6d1ab208d96744d92071f41222e7a985a22
Miguel Ugalde <migugalde@berkeley.edu>
2017-12-15 01:23:55 -0800

    Initial Commit

---
+++ Tipulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipulator
+//
+//  Created by Miguel Ugalde on 12/15/17.
+//  Copyright © 2017 Miguel Ugalde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
